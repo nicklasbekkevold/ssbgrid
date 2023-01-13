28155729ab85218408e0dbe4c9c5375bcf2d9004
Nicklas Bekkevold <44287251+nicklasbekkevold@users.noreply.github.com>
2023-01-13 11:57:49 +0100

    Create list of standard grids

---
+++ src/ssbgrid/standard_grids.py
@@ -0,0 +1,14 @@
+STANDARD_GRIDS = [
+    'SSB100M',
+    'SSB125M',
+    'SSB250M',
+    'SSB500M',
+    'SSB1KM',
+    'SSB5KM',
+    'SSB10KM',
+    'SSB25KM',
+    'SSB50KM',
+    'SSB100KM',
+    'SSB250KM',
+    'SSB500KM',
+]
