6097729aa7758b42b5a6f2247e060228b1877571
Nicklas Bekkevold <44287251+nicklasbekkevold@users.noreply.github.com>
2023-02-24 11:44:59 +0100

    Reformat init file

--- src/ssbgrid/__init__.py
+++ src/ssbgrid/__init__.py
@@ -1,4 +1,8 @@
-from ssbgrid.conversion import (align_to_grid,  # noqa: F401
-                                find_grid_cell_size, get_all_vertices,
-                                id_to_utm, utm_to_id)
-from ssbgrid.standard_grid import StandardGrid  # noqa: F401
+from .conversion import (
+    align_to_grid,
+    find_grid_cell_size,
+    get_all_vertices,
+    id_to_utm,
+    utm_to_id,
+)
+from .standard_grid import StandardGrid
